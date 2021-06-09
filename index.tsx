e248e2055c8b914a83d228f76a2cff9868cfb638
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-06-09 21:42:59 +0800

    temp

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-import Button from "@material-ui/core/Button";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
 import ReactDOM from "react-dom";
 import { LinkedStack } from "taio/build/data-structure/stack/linked-stack";
-
+import { debounce } from 'lodash-es'
 const App: React.FC = () => {
   useEffect(() => {
     const stack = new LinkedStack<number>();
@@ -10,16 +10,19 @@ const App: React.FC = () => {
     stack.push(2);
     console.log([...stack]);
   });
+  let [count, setCount] = useState(0);
+  setCount = debounce(setCount, 1000);
   return (
     <div>
-      Hello, React!
+      Hello, React count = {count}
       <Button
         onClick={() => {
-          alert("clicked!!!");
+          setCount(count => count + 1)
         }}
       >
         material UI
       </Button>
+      <div>{count}</div>
     </div>
   );
 };
